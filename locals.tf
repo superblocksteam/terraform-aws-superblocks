dee35bc257e15a59bc85edc07f2da9cd027c22ea
Taha Saifuddin <tahashabbir27@gmail.com>
2023-07-19 13:20:58 -0400

    small update

--- locals.tf
+++ locals.tf
@@ -1,8 +1,13 @@
 data "aws_region" "current" {}
 
 locals {
-  # if superblocks_agent_tags is not default, then use superblocks_agent_tags as is
-  # if superblocks_agent_tags is default, then use profile:${superblocks_agent_environment} if superblocks_agent_environment is *, else use profile:${superblocks_agent_environment}
+  # if superblocks_agent_tags is not default, then
+  #   use superblocks_agent_tags as is
+  # else if superblocks_agent_tags is default, then
+  #   if superblocks_agent_environment is *,
+  #     use profile:* (default)
+  #   else
+  #     use profile:${superblocks_agent_environment}
   superblocks_agent_tags = var.superblocks_agent_tags != "profile:*" ? var.superblocks_agent_tags : var.superblocks_agent_environment == "*" ? "profile:*" : "profile:${var.superblocks_agent_environment}"
 
   tags = merge(var.tags, {
