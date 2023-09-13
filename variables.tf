0b13d06d77ecdecbdddf8786c149ad7ce4a0e6d6
Bruce Yu <bruce@superblockshq.com>
2023-09-13 13:35:24 -0400

    test

--- variables.tf
+++ variables.tf
@@ -106,8 +106,8 @@ variable "superblocks_grpc_msg_req_max" {
 
 variable "superblocks_timeout" {
   type        = string
-  default     = "10000"
-  description = "The maximum amount of time in milliseconds before a request is aborted. This applies for http requests against the Superblocks server and does not apply to the execution time limit of a workload."
+  default     = "10000000000"
+  description = "The maximum amount of time in nanoseconds before a request is aborted. This applies for http requests against the Superblocks server and does not apply to the execution time limit of a workload."
 }
 
 variable "superblocks_log_level" {
