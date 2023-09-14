3aa2c8f6eb0282b95edf37c9cd315e712b60de44
Bruce Yu <bruce@superblockshq.com>
2023-09-14 16:26:04 -0400

    healthcheck

--- modules/load-balancer/main.tf
+++ modules/load-balancer/main.tf
@@ -14,6 +14,9 @@ resource "aws_lb_target_group" "superblocks" {
   target_type = "ip"
   vpc_id      = var.vpc_id
   tags        = var.tags
+  health_check {
+    path                = "/health"
+  }
 
   lifecycle {
     create_before_destroy = true
